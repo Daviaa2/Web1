4df2873e149a1f4e84f49ea8502d9152ab3c8aa9
AndrewD <Daviaa2@student.op.ac.nz>
2022-06-21 09:01:45 +1200

    hosting to heroku

---
+++ Assesments/Cubbits Node/routes/products.js
@@ -0,0 +1,8 @@
+const router = require("express").Router()
+let products_controller = require("../controllers/products")
+
+router.get("/", products_controller.index)
+    .get("/mens", products_controller.mens)
+    .get("/womens", products_controller.womens)
+
+module.exports = router
