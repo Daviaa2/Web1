d6a88f73d6c8623ff357117cae0358b67a1ba8fe
Andrew Davie <DAVIAA2@student.op.ac.nz>
2022-08-22 15:42:25 +1200

    fixing files

--- Assesments/Cubbits Node/routes/api.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router()
-let api_controller = require("../controllers/api")
-
-router.get("/", api_controller.index)
-    .get("/", api_controller.users)
-    .get("/", api_controller.index)
-
-module.exports = router
