388ea275c3318ca6f06e0b00dba6f546003c68dd
Andrew Davie <DAVIAA2@student.op.ac.nz>
2022-11-14 02:02:48 +1300

    padding and response fixes for the promoBare sevction

---
+++ kokako/assets/js/main.js
@@ -0,0 +1,29 @@
+// const BASE_URL = "https://api.unsplash.com/search/photos/?query=coffee&client_id=WW2w2wSFislyEtDEvaRXoV9GHobIITnYKC7E5Mprau4"
+
+
+// let div = document.querySelector(".featuredGoods_child");
+
+// fetch(BASE_URL).then(response => response.json())
+// .then(data => {
+//   console.log(data);
+//   data["results"].slice(0, 3).forEach(r => {
+//     let img = document.querySelector(".featuredGoods_img");
+//     img.src = r.["urls"]["small"];
+//     console.log(img);
+//     div.append(img);
+//   });
+// });
+
+console.log("hello ")
+
+// changing from Maori to english
+let trans = ["We're playing the long game","E purei ana mātou i te kēmu roa"]
+let trigger = true;
+
+trans.forEach( item => {   
+    console.log(item);
+    // setTimeout(() => {  console.log(item); }, 5000);
+}) 
+
+
+
